ba45b3f4a5c57dcd586564572fea25227c0b6392
coding-noble <milosdragons@gmail.com>
2023-01-07 18:11:37 -0700

    fix the year and last modified dates

--- scripts/datetime.js
+++ scripts/datetime.js
@@ -1,8 +1,7 @@
+// set the last modified when saved.
+document.getElementById("lastupdate").textContent = new Date(document.lastModified);
 
+// set the year after the copywrite simble
 let d = new Date();
 let year = d.getFullYear();
 document.getElementById("currentyear").textContent = year;
-
-let olastmodif = new Date(document.lastModified);
-
-document.getElementById("lastupdate").textContent = olastmodif;
