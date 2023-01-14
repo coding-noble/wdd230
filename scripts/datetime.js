0301aef7797866d8cd336cc52f6d8a0c5d9889d4
coding-noble <milosdragons@gmail.com>
2023-01-14 09:30:10 -0700

    Fixed datetime.js

--- scripts/datetime.js
+++ scripts/datetime.js
@@ -1,7 +1,6 @@
+// set the year after the copywrite simble
+const d = new Date();
+document.getElementById("currentyear").textContent = d.getFullYear();
+
 // set the last modified when saved.
 document.getElementById("lastupdate").textContent = new Date(document.lastModified);
-
-// set the year after the copywrite simble
-let d = new Date();
-let year = d.getFullYear();
-document.getElementById("currentyear").textContent = year;
