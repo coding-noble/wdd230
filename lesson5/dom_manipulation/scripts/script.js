e55f6f4dbecdde97eecc06dd7dcb51cc65b213ed
coding-noble <milosdragons@gmail.com>
2023-02-02 12:00:02 -0700

    W05 Learning Activity: DOM Manipulation

---
+++ lesson5/dom_manipulation/scripts/script.js
@@ -0,0 +1,23 @@
+// Get Elements
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const list = document.querySelector("ul");
+
+// Add Event Listener for Add Button
+button.addEventListener("click", () => {
+    if (input.value !== "") {
+        let li = document.createElement("li");
+        let remove = document.createElement("button");
+        li.textContent = input.value;
+        remove.textContent = "X";
+        li.append(remove);
+        list.appendChild(li);
+        remove.addEventListener("click", () => {
+            list.removeChild(li);
+        });
+        input.focus();
+        input.removeAttribute("placeholder");
+        input.removeAttribute("value");
+        input.value = ""
+    }
+});
