f7df3a9716a8eef3de677660c0769d4243744b96
coding-noble <milosdragons@gmail.com>
2023-02-04 15:30:04 -0700

    Changed Banner Day To Thursday

--- lesson5/chamber/scripts/banner.js
+++ lesson5/chamber/scripts/banner.js
@@ -3,7 +3,7 @@ let meeding_day = document.getElementById("meeting_day");
 if (d.getDay() === 1) {
     meeding_day.textContent = "Monday"
 
-} else if (d.getDay() === 3) {
-    meeding_day.textContent = "Wednesday"
+} else if (d.getDay() === 4) {
+    meeding_day.textContent = "Thursday"
 
 } else document.getElementById("meeting_banner").style.display = "none"
