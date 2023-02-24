d60df4f592909bbeff5c0a60734b0bc7ad3fdc77
coding-noble <milosdragons@gmail.com>
2023-02-24 14:32:02 -0700

    W08 Assignment: Join Page

--- lesson8/chamber/scripts/datetime.js
+++ lesson8/chamber/scripts/datetime.js
@@ -1,7 +1,11 @@
 // set the year after the copywrite simble
 const d = new Date();
+const dateTime = d.toLocaleString();
+const hiddenFormTime = document.getElementById("date-time");
 document.getElementById("currentyear").textContent = d.getFullYear();
 
 // set the last modified when saved.
 let lastUpdate = ('Last Modification: ' + document.lastModified);
 document.getElementById("lastupdate").innerHTML = lastUpdate;
+
+hiddenFormTime.setAttribute("value", dateTime);
